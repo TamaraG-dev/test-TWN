bbb41332020602f8e3876c78af0ac8b355f99943
Girard Dane <dane.girard@gmail.com>
2022-04-28 14:35:59 +0200

    Fix CORS Errors

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,17 +1,27 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core"
+import { onError } from "@apollo/client/link/error"
+import { logErrorMessages } from "@vue/apollo-util"
 
-// HTTP connection to the API
-const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  uri: '',
+// temporary
+const HASURA_SECRET = '1234';
+
+const httpLink = new HttpLink({
+    uri: "https://test-ntw.herokuapp.com/v1/graphql",
+    fetch: (uri, options) => {
+        options.headers = {
+          'x-hasura-admin-secret': HASURA_SECRET
+        }
+        return fetch(uri, options)
+    },
 })
 
-// Cache implementation
-const cache = new InMemoryCache()
+const errorLink = onError((error) => {
+    if (process.env.NODE_ENV !== "production") {
+        logErrorMessages(error)
+    }
+})
 
-// Create the apollo client
 export const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
+    cache: new InMemoryCache(),
+    link: errorLink.concat(httpLink),
 })
-
