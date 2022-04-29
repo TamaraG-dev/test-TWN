1cce516bb147d32e01f061486e4c467efc57a5a2
Girard Dane <dane.girard@gmail.com>
2022-04-29 19:10:59 +0200

    [Fix] Mutation sendContact
    Corrections et explications

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,20 +1,14 @@
 <template>
   <div v-if="!loading" class="q-pa-md flex fit justify-center text-center q-my-xl">
-    <q-table
-      class="my-sticky-header-column-table"
-      title="Contacts"
-      :rows="rows"
-      :columns="columns"
-      title-class="text-h4 text-italic"
-      hide-header
-    />
+    <q-table class="my-sticky-header-column-table" title="Contacts" :rows="rows" :columns="columns"
+      title-class="text-h4 text-italic" hide-header />
   </div>
   <div class="q-pa-xl q-mb-xl flex fit justify-center">
     <div class="q-gutter-md row items-center">
       <q-input rounded outlined v-model="Name" label="Name" />
       <q-input rounded outlined v-model="Phone" label="Phone" />
       <q-input rounded outlined v-model="email" label="Email" />
-      <q-btn color="secondary" @click="addContact()">
+      <q-btn color="secondary" @click="sendContact()">
         Add<br>contact
       </q-btn>
       <q-btn color="warning" @click="deleteContact()">
@@ -27,22 +21,22 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  import { useQuery, useResult, useMutation } from '@vue/apollo-composable'
-  import gql from 'graphql-tag'
+import { ref } from 'vue'
+import { useQuery, useResult, useMutation } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
 
-  const Name = ref('')
-  const Phone = ref('')
-  const email = ref('')
+const Name = ref('')
+const Phone = ref('')
+const email = ref('')
 
 
-  const columns = [
-    { name: 'Name', align: 'center', label: 'Name', field: 'Name', sortable: true },
-    { name: 'Phone', align: 'center', label: 'Phone', field: 'Phone', sortable: true },
-    { name: 'email', align: 'center', label: 'email', field: 'email', sortable: true },
-  ]
+const columns = [
+  { name: 'Name', align: 'center', label: 'Name', field: 'Name', sortable: true },
+  { name: 'Phone', align: 'center', label: 'Phone', field: 'Phone', sortable: true },
+  { name: 'email', align: 'center', label: 'email', field: 'email', sortable: true },
+]
 
-  const { result, loading } = useQuery(gql`
+const { result, loading } = useQuery(gql`
     query MyQuery {
       TestTable {
         Name
@@ -52,36 +46,66 @@
         }
       }`)
 
-  const rows = useResult(result, null, data => data.TestTable)
+const rows = useResult(result, null, data => data.TestTable)
 
-  /*function onDone() {
-    newMessage.value = 'onDone'
-  })*/
+/*function onDone() {
+  newMessage.value = 'onDone'
+})*/
 
-  function addContact() {
-    const { mutate: sendContact, loading } = useMutation(gql`
-      mutation sendContact {
-        insert_TestTable_one(object: {Name: $Name, Phone: $Phone, email: $email}, on_conflict: {constraint: TestTable_pkey, where: {id: {}}}) {
-          id
-        }
-      }`)
-      console.log(loading)
+
+//        >>>>> Explications <<<<< 
+// 
+// La syntaxe : const { mutate: sendContact } 
+// te permet de pouvoir appeler ta mutation comme une fonction
+// Donc pas besoin de l'encapsuler dans une fonction
+// Ensuite, pour utiliser des variables dans une mutation, cette syntaxe n'est pas correcte : 
+// 
+//  const { mutate: sendContact, loading } = useMutation(gql`
+//   mutation sendContact {
+//     insert_TestTable_one(object: {Name: $Name, Phone: $Phone, email: $email}, on_conflict: {constraint: TestTable_pkey, where: {id: {}}}) {
+//       id
+//     }
+//   }
+// `)
+// 
+// Il faut que tu déclare en paramètres de ta mutation les paramètres qu'elles doit recevoir (Nom + Type)
+// et ensuite, useMutation recois en argument ta requete, et les paramètres qui doivent être passer a la requete 
+// useMutation(document (correspond a ta requete), options (ou tu retrouves "variables"))
+// cf :  https://v4.apollo.vuejs.org/api/use-mutation.html#parameters
+// ce qui donne : 
+
+const { mutate: sendContact } = useMutation(gql`
+  mutation INSERT_ONE_USER ($Name: String, $Phone: numeric, $email: String) { 
+    insert_TestTable_one(object: {Name: $Name, Phone: $Phone, email: $email}) {
+      id
+    }
+  }`, () => ({
+    variables: {
+      Name: Name.value,
+      Phone: Phone.value,
+      email: email.value
+    }
   }
-  console.log(rows)
+  )
+)
 
- 
-  function deleteContact() {
-    if(rows.value.length > 1) {
-      console.log("delete contact")
-      const lastId = rows.value[rows.value.length - 1].id
 
-      const { mutate } = useMutation(gql`
+// Je te laisse modifier la partie delete contact ;)
+// Il faudra surement que tu revois ta logique pour déterminer quel contact tu veux supprimer
+// tu peux par exemple définir ta mutation en dehors de ta fonction deleteContact, laisser la logique dedans et ensuite appeler ta mutation depuis cetet fonction
+
+function deleteContact() {
+  if (rows.value.length > 1) {
+    console.log("delete contact")
+    const lastId = rows.value[rows.value.length - 1].id
+
+    const { mutate } = useMutation(gql`
         mutation MyMutation {
           delete_TestTable_by_pk(id: $lastId) {
             id
           }
         }`)
-    } 
-
   }
+
+}
 </script>
